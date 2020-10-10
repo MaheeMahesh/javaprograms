a4a450a39c624cfdae10f5d0bdc1f45a9ef9666c
MaheeMahesh <70712896+MaheeMahesh@users.noreply.github.com>
2020-10-10 19:36:10 +0530

    Add files via upload

---
+++ Employee/Employee.java
@@ -0,0 +1,14 @@
+package com.Employee;
+
+public class Employee
+{
+    public String name;
+    public String age;
+    public String city;
+    public void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
+
---
+++ Employee/Main.java
@@ -0,0 +1,13 @@
+package com.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.name = "The Name is mahesh";
+        e1.age =  "The Age is "+ 22;
+        e1.city="The City is Hyderabad";
+        e1.display();
+    }
+}
+    
